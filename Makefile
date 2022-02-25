2c2522c86c2af7bb30d473387bdafc4e2dacf660
willholl4nd <wylie1027@comcast.net>
2022-02-25 18:02:31 -0500

    Fixed bug where commands.sh script did not work

--- Makefile
+++ Makefile
@@ -15,10 +15,18 @@ bitrates:
 	mv opus/*.mp3 mp3
 
 tags:
-	./commands.sh
+	mv commands.sh mp3&
+	mv redoId3v2.py mp3&
+	mv moveSongs.py mp3&
+	cd mp3 && ./commands.sh
+	mv mp3/commands.sh .&
+	mv mp3/redoId3v2.py .&
+	mv mp3/moveSongs.py .&
 
 
 clean:
 	rm titles.txt paths.txt bitrates.txt& 
 	rm -rf opus mp3
 	mkdir opus mp3
+	touch opus/.gitkeep
+	touch mp3/.gitkeep
--- commands.sh
+++ commands.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 #Song format = ARTIST - TITLE
 
-#First step: Copy all of newly downloaded music to current directory
+#First step: Move three scripts to mp3 folder via Makefile
 
 #Second: Clear all of the ID3 Tags
 id3v2 -D *.mp3
 
 #Third step: Uses the below command to grab the artist name and create folders
-ls *-* | awk -F ' - ' '{ print $1 }' | xargs -I {} -L 1 mkdir "{}"
+#ls *-* | awk -F ' - ' '{ print $1 }' | xargs -I {} -L 1 mkdir "{}"
+ls *-* | awk -F ' - ' '{print $1}' | xargs -I '{}' mkdir '{}'
 
 #Fourth: Run python script to add corrected id3v2 tags
-python redoId3v2.py
+python3 redoId3v2.py
 
 #Fifth: Move all songs to correct folder
-python moveSongs.py
+python3 moveSongs.py
 
+#Sixth: Move three scripts back to main folder via Makefile
